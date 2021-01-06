d228c4c7f88d2bec99916e7382344f10f979f5f4
RoyP <royprather@protonmail.com>
2021-01-05 21:09:19 -0800

    bugfix

--- dieBot.py
+++ dieBot.py
@@ -52,31 +52,36 @@ async def on_message(msg):
                     elif isint(msg.content[5]):
                         await msg.channel.send(str(rollDice(num,msg.content[3],msg.content[5])))
                 #is there a two digit number with no + sign
-                elif isint(msg.content[3:5]):
+                if isint(msg.content[3:5]):
                     await msg.channel.send(str(rollDice(num,msg.content[3:5],0)))
                 #is there a one digit number with no + sign
                 elif isint(msg.content[3]):    
                     await msg.channel.send(str(rollDice(num,msg.content[3],0)))
+        
         #roll up to 9 d 100
         elif msg.content[1:5] == 'd100':
             await msg.channel.send(str(rollDice(num,100,0)))
         #is the second index a d    
         elif msg.content[1] == 'd':
-            #is the third and forth indexs a number and is the fith index a + sign
-            if isint(msg.content[2:4]) and msg.content[4] == '+':
-                #is there a two digit number fallowing the + sign
-                if isint(msg.content[5:7]):
-                    await msg.channel.send(str(rollDice(num,msg.content[2:4],msg.content[5:7])))
-                #is there a one digit number fallowingt the + sign
-                elif isint(msg.content[5]):
-                    await msg.channel.send(str(rollDice(num,msg.content[2:4],msg.content[5])))
-            #is the third index a number and the forth index a + sign    
-            elif isint(msg.content[2]) and msg.content[3] == '+':
-                #is there a two digit number fallowing the + sign
-                if isint(msg.content[4:6]):
-                    await msg.channel.send(str(rollDice(num,msg.content[2],msg.content[4:6])))
-                #is there a one digit number fallowing the + sign
-                elif isint(msg.content[4]):
+            #is the third and forth indexs a number 
+            if isint(msg.content[2:4]): 
+                #is the fifth index a + sign
+                if msg.content[4] == '+':
+                    #is there a two digit number fallowing the + sign
+                    if isint(msg.content[5:7]):
+                        await msg.channel.send(str(rollDice(num,msg.content[2:4],msg.content[5:7])))
+                    #is there a one digit number fallowingt the + sign
+                    elif isint(msg.content[5]):
+                        await msg.channel.send(str(rollDice(num,msg.content[2:4],msg.content[5])))
+            #is the third index a number    
+            elif isint(msg.content[2]):
+                #is the fourth index a +
+                if msg.content[3] == '+':
+                    #is there a two digit number fallowing the + sign
+                    if isint(msg.content[4:6]):
+                        await msg.channel.send(str(rollDice(num,msg.content[2],msg.content[4:6])))
+                    #is there a one digit number fallowing the + sign
+                    elif isint(msg.content[4]):
                         await msg.channel.send(str(rollDice(num,msg.content[2],msg.content[4])))
             #is there a two digit number with no + sign
             elif isint(msg.content[2:4]):
